c8f921e135106530f5b8fd33344c1df560d099d4
FIDGE <fidge.off2290@gmail.com>
2024-10-21 13:03:25 +0300

    Виправлені деякі помилки

--- Java Skript.js
+++
@@ -1,9 +0,0 @@
-// Плавний скрол до секцій при натисканні на пункти меню
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
