38949a5e1c725567ccd60327cc00b56a78a0ddec
FIDGE <fidge.off2290@gmail.com>
2024-10-21 09:44:37 +0300

    deleted: Logo.png,LabCss.css,LabSite,page2.html,page3.html. modified: LabSite.html

---
+++ Java Skript.js
@@ -0,0 +1,9 @@
+// Плавний скрол до секцій при натисканні на пункти меню
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
